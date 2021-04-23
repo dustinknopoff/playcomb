e386d0cefee3f0c576152771b91b8d3e7f9c645f
Dustin Knopoff <dustin@knopoff.dev>
2021-04-23 14:14:53 -0400

    remove google font fetch

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,18 +36,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Roboto`,
-            variants: ["400", "700"],
-          },
-        ],
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
+       // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
